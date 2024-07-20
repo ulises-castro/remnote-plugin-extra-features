16dfbdc77f20b72241b3ec6f5ad6d5bde79d3893
Ulises Castro <ulises.ucastro@gmail.com>
2024-07-19 22:31:30 -0600

    feat: add first command and increase permissions

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,45 +1,32 @@
-import { declareIndexPlugin, ReactRNPlugin, WidgetLocation } from '@remnote/plugin-sdk';
+import { declareIndexPlugin, ReactRNPlugin, RichTextElementTextInterface, RichTextInterface, WidgetLocation } from '@remnote/plugin-sdk';
 import '../style.css';
 import '../App.css';
 
 async function onActivate(plugin: ReactRNPlugin) {
-  // Register settings
-  await plugin.settings.registerStringSetting({
-    id: 'name',
-    title: 'What is your Name?',
-    defaultValue: 'Bob',
-  });
-
-  await plugin.settings.registerBooleanSetting({
-    id: 'pizza',
-    title: 'Do you like pizza?',
-    defaultValue: true,
-  });
-
-  await plugin.settings.registerNumberSetting({
-    id: 'favorite-number',
-    title: 'What is your favorite number?',
-    defaultValue: 42,
-  });
-
   // A command that inserts text into the editor if focused.
   await plugin.app.registerCommand({
-    id: 'editor-command',
-    name: 'Editor Command',
+    id: 'insert-codeblock-js',
+    name: 'Insert Codeblock Javascript',
     action: async () => {
-      plugin.editor.insertPlainText('Hello World!');
+
+      const newRemCodeBlock: RichTextInterface = [{
+        code: true,
+        text: "",
+        i: "m"
+      }]
+
+      plugin.editor.insertRichText(newRemCodeBlock)
+      // plugin.editor.insertPlainText('Hello World!');
     },
   });
 
-  // Show a toast notification to the user.
-  await plugin.app.toast("I'm a toast!");
 
-  // Register a sidebar widget.
-  await plugin.app.registerWidget('sample_widget', WidgetLocation.RightSidebar, {
-    dimensions: { height: 'auto', width: '100%' },
-  });
+  // // Register a sidebar widget.
+  // await plugin.app.registerWidget('sample_widget', WidgetLocation.RightSidebar, {
+  //   dimensions: { height: 'auto', width: '100%' },
+  // });
 }
 
-async function onDeactivate(_: ReactRNPlugin) {}
+async function onDeactivate(_: ReactRNPlugin) { }
 
 declareIndexPlugin(onActivate, onDeactivate);

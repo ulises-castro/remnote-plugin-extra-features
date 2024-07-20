c412f0471f8eb1cc346077f15625f9daaca7329d
Ulises Castro <ulises.ucastro@gmail.com>
2024-07-20 16:59:00 -0600

    feat: add commans to insert codeblock by programming language

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -2,29 +2,59 @@ import { declareIndexPlugin, ReactRNPlugin, RichTextElementTextInterface, RichTe
 import '../style.css';
 import '../App.css';
 
+const LANGUAGES_OPT = [
+  ['Javascript', { code: 'js' }],
+  ['Typescript', {
+    code: 'ts',
+  }],
+  ['Python', {
+    code: 'py',
+  }],
+  ['JSON', {
+    code: 'json',
+  }],
+  ['SQL', {
+    code: 'sql',
+  }],
+  ['HTML', {
+    code: 'html',
+  }],
+  ['CSS', {
+    code: 'css',
+  }]]
+
+const LanguageOptionsMap = new Map<string, { code: string }>(LANGUAGES_OPT)
+
+async function registerCommands(plugin: ReactRNPlugin) {
+  // TODO: Pending to request permissions to copy from clipboard
+  const insertCodeBlockByLanguage = async (text?: string = ' ', language: string, insertFromClipboardContent: boolean = false) => {
+    let codeContent: string = text;
+    if (insertFromClipboardContent) {
+    }
+
+    const newRemCodeBlock = await plugin.richText.code(codeContent, 'javascript').value()
+    plugin.editor.insertRichText(newRemCodeBlock)
+  }
+
+  for (const [language, values] of LanguageOptionsMap.entries()) {
+
+    await plugin.app.registerCommand({
+      id: 'insert-from-codeblock-' + language,
+      quickCode: "c" + values.code,
+      name: `Insert Clipboard content into a Codeblock ${language}`,
+      action: async () => {
+
+        const newRemCodeBlock = await plugin.richText.code(' ', language.toLowerCase()).value()
+        plugin.editor.insertRichText(newRemCodeBlock)
+      },
+    });
+  }
+
+}
+
 async function onActivate(plugin: ReactRNPlugin) {
-  // A command that inserts text into the editor if focused.
-  await plugin.app.registerCommand({
-    id: 'insert-codeblock-js',
-    name: 'Insert Codeblock Javascript',
-    action: async () => {
 
-      const newRemCodeBlock: RichTextInterface = [{
-        code: true,
-        text: "",
-        i: "m"
-      }]
-
-      plugin.editor.insertRichText(newRemCodeBlock)
-      // plugin.editor.insertPlainText('Hello World!');
-    },
-  });
-
-
-  // // Register a sidebar widget.
-  // await plugin.app.registerWidget('sample_widget', WidgetLocation.RightSidebar, {
-  //   dimensions: { height: 'auto', width: '100%' },
-  // });
+  await registerCommands(plugin);
 }
 
 async function onDeactivate(_: ReactRNPlugin) { }
